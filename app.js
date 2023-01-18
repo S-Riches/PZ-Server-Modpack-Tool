ef342c98702845350bbc828a4370171ed3a20181
S-Riches <samuelrichesprivate@gmail.com>
2023-01-18 00:10:47 +0000

    trying to work out the steam web api

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
+import fetch from "node-fetch";
+
+// Grabs the collection id
+const getIdsFromCollection = async (collectionId) => {
+    const url = `https://api.steampowered.com/ISteamRemoteStorage/GetCollectionDetails/v1/`;
+    const options = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: {
+            key: "...",
+            collectioncount: "1",
+            "publishedfileids[0]": "2916244611",
+        },
+    };
+    const response = await fetch(url, options);
+    const data = await response;
+    console.log(data);
+};
+
 const main = async () => {
     // main runner code
+    getIdsFromCollection();
 };
 
 main();
